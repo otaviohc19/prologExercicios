360e07ab47c8963988e552420ef7b0e5d49fab17
otaviohc19 <otaviohcrrodrigues@gmail.com>
2023-05-25 16:18:08 -0300

    Atividade2

---
+++ sonhos.pl
@@ -0,0 +1,11 @@
+/*Noite de sonhos
+ * Depois de assistirem a vários filmes de aventura num final de semana, Juliano e seus dois irmãos sonharam
+ * com aquele mundo de fantasias. Um dos irmãos se chama Nelson. Um dos locais é uma floresta e uma das entidades
+ * é um dragão.
+ * Com base nas dicas, descubra o nome dos garotos, a entidade que cada um deles encontrou no sonho e o local onde
+ * estavam.
+ * 1. Roberto sonhou que encontrava um unicórnio.
+ * 2. Um dos garotos sonhou que encontrava um bruxo num pântano.
+ * 3. Juliano sonhou que estava num castelo.
+ * 
+ * */
